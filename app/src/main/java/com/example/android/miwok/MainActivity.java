f99c3a4bc4723c8a9366f7cff3fde7a2224e3975
sinithwar <sinithwar@yahoo.com>
2016-11-09 23:21:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the numbers category
+        final TextView numbers = (TextView) findViewById(R.id.numbers);
+        final TextView family = (TextView) findViewById(R.id.family);
+        final TextView colors = (TextView) findViewById(R.id.colors);
+        final TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        // Set a click listener on that View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent viewIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(viewIntent);
+            }
+        });
+        family.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent viewIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(viewIntent);
+            }
+        });
+        colors.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent viewIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(viewIntent);
+            }
+        });
+        phrases.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent viewIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(viewIntent);
+            }
+        });
+    }
+}
